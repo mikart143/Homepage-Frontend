6e03063a6042c918daa2a9c95af0a7ede043ccbc
Michael Kruczek <mikart143@gmail.com>
2022-06-12 23:15:00 +0200

    Added universal console output and runtime linting

--- src/src/main.ts
+++ src/src/main.ts
@@ -3,17 +3,12 @@ import {createPinia} from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import consola from 'consola';
+
+consola.wrapAll();
 
 const app = createApp(App);
-app.config.errorHandler = (err) => {
-    console.error(`ERROR|${new Date().toISOString()}: `, err);
-};
-
-app.config.warnHandler = (warn) => {
-    console.warn(`WARN|${new Date().toISOString()}: `, warn);
-};
 
 app.use(createPinia());
 app.use(router);
-
 app.mount('#app');
